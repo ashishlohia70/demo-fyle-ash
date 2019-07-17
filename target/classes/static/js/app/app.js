687438d17d5c8c6d55d41da2a8dcbf4261580a4a
Ashish Lohia <ashish.lohia@wrkspot.com>
2019-07-17 21:16:14 +0530

    removed target

--- target/classes/static/js/app/app.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-var demoApp = angular.module('demo', [ 'ui.bootstrap', 'ui.grid',
-		'ui.grid.pagination', 'ui.grid.resizeColumns', 'demo.controllers',
-		'demo.services', 'cgBusy']);
-
-demoApp.constant("CONSTANTS", {
-	getCities : "/banks/getCities",
-	findByCity : "/banks/findByCity"
-});
